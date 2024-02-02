943368b8e7e318563b7f3308bc3e4bbad3235a4a
GregSal <33795111+GregSal@users.noreply.github.com>
2024-02-01 20:23:18 -0500

    After update to python 3.12

--- Environment/VS Code (SectionaryDev).bat
+++ Environment/VS Code (SectionaryDev).bat
@@ -3,9 +3,10 @@ rem set WORKSPACE_FOLDER="%HOMEPATH%\OneDrive - Queen's University\Python\Projec
 set WORKSPACE_FOLDER="D:\OneDrive - Queen's University\Python\Projects\sectionary package"
 set WORKSPACE_FILE="%WORKSPACE_FOLDER%\sectionary.code-workspace"
 
-CALL C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+CALL D:\anaconda3\Scripts\activate.bat D:\anaconda3
 CALL conda activate sectionaryDev
 Cd "%WORKSPACE_FOLDER%"
 D:
 CALL code sectionary.code-workspace
 EXIT
+
